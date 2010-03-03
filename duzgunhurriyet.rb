86248c2e33c800a54faa8bd2831e56ab6fc0de6b
Arda Çınar <kuzux92@gmail.com>
2010-03-03 18:27:17 +0200

    Added right-hand headlines

--- duzgunhurriyet.rb
+++ duzgunhurriyet.rb
@@ -21,6 +21,13 @@ def mansetler
     links << Manset.new(img,nil,link)
   end
   
+  1.upto(4) do |i|
+    div = doc.css("#hurriyet_4lu_#{i}")
+    img = div.css('img')[0]['src']
+    link = div.css('a')[0]['href']
+    links << Manset.new(img, nil, link)
+  end
+
   doc.css(".mansetImageDiv").each do |e|
     img = e.children.css('img')[0]['src']
     link = e.children.css('a')[0]['href']
